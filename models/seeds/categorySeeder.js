c21b5394d05f7b919689d158989c7160c9da5f44
aziz0916 <aziz0916@yahoo.com.tw>
2022-04-03 00:29:26 +0800

    feat: add category, user and record seeder

---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,11 @@
+const db = require('../../config/mongoose')
+const Category = require('../category')
+
+const CATEGORY = [{ name: '家居物業' }, { name: '交通出行' }, { name: '休閒娛樂' }, { name: '餐飲食品' }, { name: '其他' }]
+
+db.once('open', () => {
+  Category.create(CATEGORY)
+    .then(() => {
+      process.exit()
+    })
+})
---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,37 @@
+const db = require('../../config/mongoose')
+const Category = require('../category')
+const Record = require('../record')
+const User = require('../user')
+const recordList = require('../../record.json').results
+
+const SEED_USER = {
+  name: '廣志'
+}
+
+async function createSeed(userId, record, categoryName) {
+  try {
+    const category = await Category.findOne({ name: categoryName })
+    const categoryId = category._id
+    record.userId = userId
+    record.categoryId = categoryId
+    await Record.create(record)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+db.once('open', async () => {
+  try {
+    const user = await User.create({
+      name: SEED_USER.name,
+    })
+    const userId = user._id
+    for (let i = 0; i < recordList.length; i++) {
+      await createSeed(userId, recordList[i], recordList[i].category)
+    }
+    console.log('done')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+  }
+})
