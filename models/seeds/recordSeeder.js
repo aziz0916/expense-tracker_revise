c16da0182b2636647023e068d2dfa2d11098ebda
aziz0916 <aziz0916@yahoo.com.tw>
2022-04-05 00:49:03 +0800

    modify categorySeeder and recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,3 +1,9 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
 const Category = require('../category')
 const Record = require('../record')
@@ -5,7 +11,9 @@ const User = require('../user')
 const recordList = require('../../record.json').results
 
 const SEED_USER = {
-  name: '廣志'
+  name: '廣志',
+  email: 'user1@example.com',
+  password: '12345678'
 }
 
 async function createSeed(userId, record, categoryName) {
@@ -22,8 +30,12 @@ async function createSeed(userId, record, categoryName) {
 
 db.once('open', async () => {
   try {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(SEED_USER.password, salt)
     const user = await User.create({
       name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
     })
     const userId = user._id
     for (let i = 0; i < recordList.length; i++) {
